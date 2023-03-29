fe59ba00d4ce01f86ec53100239861e177243aa0
Nika Vrpoljac <nika.vrpoljac95@gmail.com>
2023-03-29 21:48:00 +0200

    mobile version added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const avatars = ['😄', '😊', '😎', '🙄', '👻', '🤠', '😋', '🥰'];
 
 const Login = ({ setUser }) => {
-
   const [username, setUsername] = useState('');
   const [avatar, setAvatar] = useState(avatars[0]);
+  const selectedAvatar = useRef(null);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
+  };
+
+  function handleAvatarChange(event) {
+    setAvatar(event.target.value);
+    const selected = selectedAvatar.current;
+    if (selected) {
+      selected.style.backgroundColor = "";
+    }
+    event.target.parentNode.classList.add("selected");
+    selectedAvatar.current = event.target.parentNode;
   }
 
-
-  const handleAvatarChange = (event) => {
-    setAvatar(event.target.value)
+  const handleAvatarClick = (event) => {
+    const selected = selectedAvatar.current;
+    if (selected) {
+      selected.style.backgroundColor = "";
+    }
+    event.target.style.backgroundColor = "rgb(226, 134, 150)";
+    selectedAvatar.current = event.target;
+    setAvatar(event.target.textContent);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(username !== "" && avatar !== ""){
-      setUser({username: username, avatar: avatar});
+    if (username !== "" && avatar !== "") {
+      setUser({ username: username, avatar: avatar });
     }
   }
 
@@ -29,30 +44,36 @@ const Login = ({ setUser }) => {
         <span className="logo">Chat App</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="username" value={username} onChange={handleUsernameChange} />
-            <ul className="selectAvatar">
-              {avatars.map((a, index) => (
-                <li key={"avatar"+index}>
-                  <label>
-                    <input
-                      type="radio"
-                      name="avatar"
-                      value={a}
-                      checked={avatar === a}
-                      onChange={handleAvatarChange}
-                    />
+          <input className="inputUsername" type="text" placeholder="username" value={username} onChange={handleUsernameChange} />
+          <ul className="selectAvatar">
+            {avatars.map((a, index) => (
+              <li key={"avatar" + index}>
+                <label>
+                  <input
+                    className="emoji"
+                    type="radio"
+                    name="avatar"
+                    value={a}
+                    checked={avatar === a}
+                    onChange={handleAvatarChange}
+                    style={{ display: "none" }}
+                  />
+                  <span
+                    className={"avatar" + (avatar === a ? " selected" : "")}
+                    onClick={handleAvatarClick}
+                  >
                     {a}
-                  </label>
-                </li>
-              ))}
-            </ul>        
-            <button type="submit">Enter chat</button>
+                  </span>
+                </label>
+              </li>
+            ))}
+          </ul>
+          <button className="enterChat" type="submit">Enter chat</button>
         </form>
-        
       </div>
     </div>
   );
 };
 
-//label zapravo zamjenjuje 'odaberi datoteku'button i moze biti slika
-export default Login;
+
+export default Login
